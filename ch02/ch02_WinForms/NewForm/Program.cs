6497a71a33c5b3acfec6c4430b2ef7c48aca21db
David Stevenson <drstevenson1958@yahoo.com>
2024-10-06 15:10:19 -0400

    Chapter 2: The Form

---
+++ ch02/ch02_WinForms/NewForm/Program.cs
@@ -0,0 +1,14 @@
+namespace NewForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new Form();
+        }
+    }
+}

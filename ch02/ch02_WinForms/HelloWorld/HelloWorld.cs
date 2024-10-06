25492e492f6a2acd499d9cff64cca7675212c675
David Stevenson <drstevenson1958@yahoo.com>
2024-10-06 17:36:34 -0400

    Chapter 2: Hello World using the OnPaint method

---
+++ ch02/ch02_WinForms/HelloWorld/HelloWorld.cs
@@ -0,0 +1,24 @@
+namespace HelloWorld
+{
+    class HelloWorld : Form
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new HelloWorld());
+        }
+
+        public HelloWorld()
+        {
+            Text = "Hello World";
+            BackColor = Color.White;
+        }
+
+        protected override void OnPaint(PaintEventArgs pea)
+        {
+            Graphics grfx = pea.Graphics;
+
+            grfx.DrawString("Hello, Windows Form!", Font, Brushes.Black, 0, 0);
+        }
+    }
+}
